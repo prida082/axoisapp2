064be62daf1d28a3cca1cff9f09829d31e0b691b
prida082 <beatbrave882@gmail.com>
2021-03-05 15:22:08 +0700

    axiosmanga1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info" class="navbar1">
+      <b-navbar-brand href="#"><h2 class="font1">MANGA</h2></b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <!-- <b-nav-form>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              placeholder="Search"
+            ></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit"
+              >Search</b-button
+            >
+          </b-nav-form> -->
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col cols="3" v-for="ghost in items.characters" :key="ghost" >
+          <b-card
+            :title="ghost.name"
+            :img-src="ghost.image_url"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem"
+            class="mb-2"
+          >
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  props:{
+    title: String,
+    image: String
+  },
+  data() {
+    return {
+      items: "",
+    };
+  },
+  mounted() {
+    axios({
+      method: "GET",
+      url: "https://jikan1.p.rapidapi.com/manga/2/characters",
+      headers: {
+        "x-rapidapi-key": "3be2405cd7mshd7ba84867da62f6p1a5629jsn324c16641a35",
+        "x-rapidapi-host": "jikan1.p.rapidapi.com",
+      },
+    }).then((res) => {
+      this.items = res.data;
+    });
+  },
 };
 </script>
+
+<style>
+.bg-info {
+  background-color: #a813ff !important;
+}
+.navbar1 {
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.5rem 1rem;
+  height: 80px;
+}
+.btn-group-sm > .btn,
+.btn-sm {
+  padding: 0.25rem 0.5rem;
+  font-size: 1.275rem;
+  line-height: 1.5;
+  border-radius: 0.2rem;
+}
+.btn-secondary {
+  color: rgb(0, 0, 0);
+  background-color: #007fee;
+  border-color: #00c3ff;
+}
+.form-inline .form-control {
+  display: inline-block;
+  width: 260px;
+  height: 38px;
+  vertical-align: middle;
+}
+.font1 {
+  color: rgb(91, 236, 255);
+}
+</style>
